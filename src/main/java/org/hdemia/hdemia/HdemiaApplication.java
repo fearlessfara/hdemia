facd79e7da6c6bf317f5b0b88777218ded6ab993
cfaraone <cfaraone@soldo.com>
2022-04-01 15:52:40 +0200

    I'm batman. This commit has no parents.

---
+++ src/main/java/org/hdemia/hdemia/HdemiaApplication.java
@@ -0,0 +1,13 @@
+package org.hdemia.hdemia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdemiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdemiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hdemia/hdemia/HdemiaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hdemia.hdemia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdemiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
