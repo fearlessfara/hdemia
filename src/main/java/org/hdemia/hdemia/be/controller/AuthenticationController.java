be14824ae98970c3979775b6f2088e8676b44f71
Badr <badr.elmazaz@eng.it>
2022-04-01 19:54:16 +0200

    added activemq artemis in dockercompose

--- src/main/java/org/hdemia/hdemia/be/controller/AuthenticationController.java
+++ src/main/java/org/hdemia/hdemia/be/controller/AuthenticationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("v1/auth")
 public class AuthenticationController {
 
     @Autowired
--- src/main/java/org/hdemia/hdemia/be/controller/BookingController.java
+++ src/main/java/org/hdemia/hdemia/be/controller/BookingController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/booking")
+@RequestMapping("/v1/booking")
 public class BookingController {
 }
